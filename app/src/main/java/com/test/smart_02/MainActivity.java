59b8cb8a14050bf44995054f61e4120305898584
qudymao@foxmail.com <Qm62481004>
2018-05-10 11:40:17 +0800

    新增加功能

---
+++ app/src/main/java/com/test/smart_02/MainActivity.java
@@ -0,0 +1,22 @@
+package com.test.smart_02;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+
+    private void initView(){
+    }
+
+}
